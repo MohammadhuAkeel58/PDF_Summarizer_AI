7255ec8f580aaca3552caf55ec8af91056339f04
Mohamed Akeel <akeelaslamcse@gmail.com>
2025-06-26 09:33:01 +0530

    project setup and eslint config

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Source_Sans_3 as Font_sas } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const Source_Sans_3 = Font_sas({
+  variable: "--font-sas",
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
 export const metadata: Metadata = {
@@ -24,9 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={` font-sas ${Source_Sans_3.variable} antialiased`}>
         {children}
       </body>
     </html>
